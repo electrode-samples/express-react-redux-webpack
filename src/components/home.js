33af3eac3af625dfee8b000db4f9f09da7d7dfb5
raydecastro <captray108@gmail.com>
2016-09-16 20:56:20 -0700

    docs. removed the button tags for consistency.

--- src/components/home.js
+++ src/components/home.js
@@ -8,27 +8,21 @@ export class Home extends React.Component {
         <h2>Demonstration Components</h2>
         <ul>
           <li className="aboveFold">
-            <button>
-              <a href="/above-the-fold">
-                Above the Fold Render
-              </a>
-            </button>
+            <a href="/above-the-fold">
+              Above the Fold Render
+            </a>
             <p>Increase your App's performance by using a skip prop</p>
           </li>
           <li className="ssr simple">
-            <button>
-              <a href="/ssrcachingsimpletype">
+            <a href="/ssrcachingsimpletype">
               SSR Caching - Simple
-              </a>
-            </button>
+            </a>
             <p>Component Props become the cache key. This is useful for cases like Header and Footer where the number of variations of props data is minimal which will make sure the cache size stays small.</p>
           </li>
           <li className="ssr caching">
-            <button>
-              <a href="/ssrcachingtemplatetype">
-                SSR Caching- Template Type
-              </a>
-            </button>
+            <a href="/ssrcachingtemplatetype">
+              SSR Caching- Template Type
+            </a>
             <p>Components Props are first tokenized and then the generated template html is cached. The idea is akin to generating logic-less handlebars template from your React components and then use string replace to process the template with different props. This is useful for cases like displaying Product information in a Carousel where you have millions of products in the repository.</p>
           </li>
         </ul>
